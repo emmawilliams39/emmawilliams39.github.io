7251e163e89bcf2241fec361b7ea548b02e4e609
emmawilliams39 <emmaamedio@gmail.com>
2025-06-16 20:15:28 +0000

    I created and edited Runtime

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,9 +18,88 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+      function createSawBlade (x, y, damage){
+        var hitZoneSize = 25;
+        var damageFromObstacle = damage;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+        sawBladeHitZone.x = x;
+        sawBladeHitZone.y = y;
+        game.addGameItem(sawBladeHitZone);
+        var obstacleImage = draw.bitmap("img/sawblade.png");
+        obstacleImage.x = -25
+        obstacleImage.y = -25
+        sawBladeHitZone.addChild(obstacleImage);
+      }
 
-    
+      function createEnemy(x, y, speed){
+        var enemy = game.createGameItem("enemy", 25);
+        var redSquare = draw.rect(50, 50, "red");
+        redSquare.x = -25;
+        redSquare.y = -25;
+        enemy.addChild(redSquare);
+        enemy.x = x;
+        enemy.y = y;
+        game.addGameItem(enemy);
+        enemy.velocityX = speed;
+        enemy.onPlayerCollision = function () {
+        game.changeIntegrity(-10) 
+        };
+        enemy.onProjectileCollision = function(){
+          game.increaseScore(100);
+          enemy.fadeOut();
+        };
 
+      }
+       function createReward(x, y, speed){
+        var reward = game.createGameItem("reward", 25);
+        var blueSquare = draw.rect(50, 50, "blue");
+        blueSquare.x = -25;
+        blueSquare.y = -25;
+        reward.addChild(blueSquare);
+        reward.x = x;
+        reward.y = y;
+        game.addGameItem(reward);
+        reward.velocityX = speed;
+        reward.onPlayerCollision = function () {
+        game.changeIntegrity(10) 
+        };
+        reward.onProjectileCollision = function(){
+          game.increaseScore(100);
+          reward.fadeOut();
+        };
+
+      }
+      function createMarker(x, y, speed){
+        var marker = game.createGameItem("marker", 25);
+        var yellowSquare = draw.rect(50, 50, "yellow");
+        yellowSquare.x = -25;
+        yellowSquare.y = -25;
+        marker.addChild(yellowSquare);
+        marker.x = x;
+        marker.y = y;
+        game.addGameItem(marker);
+        marker.velocityX = speed;
+        marker.onPlayerCollision = function () {
+        game.changeIntegrity(10) 
+         startLevel();
+        };
+        marker.onProjectileCollision = function(){
+          game.increaseScore(100);
+          marker.fadeOut();
+          startLevel();
+        };
+
+      }
+      
+
+      //function calls
+      createSawBlade(400, groundY - 130, 20);
+      createSawBlade(800, groundY - 10, 5);
+      createEnemy(400, groundY - 30, -3);
+      createEnemy(1300, groundY - 30, -1);
+      createEnemy(1700, groundY - 30, -2);
+      createReward(1000, groundY - 50, -2);
+      createMarker(900, groundY - 75, -3); 
     function startLevel() {
       // TODO 13 goes below here
 
--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -29,7 +29,15 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
+        var tree; 
+        var buildings = []; 
+
+
+
+
+
+
+
       
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -39,17 +47,41 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY, 'lightblue');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
-            
+            for(var i = 0;  i < 100; i++){
+            var circle = draw.circle(2, "white", "LightGray", 2);
+            circle.x = canvasWidth * Math.random();
+            circle.y = groundY * Math.random();
+            background.addChild(circle);
+            }
+
+
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = canvasWidth - 240;
+            moon.y = groundY - 440;
+            moon.scaleX = 0.5;
+            moon.scaleY = 0.5;
+            background.addChild(moon);
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; ++i) {
+                var buildingColors = ["darkblue", "darkgrey", "grey", "blue", "lightgrey"]
+                var buildingHeight = 200 * Math.random();
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+            }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 290;
+            tree.y = 220;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -63,10 +95,20 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 3;
+            if (tree.x < -200) {
+             tree.x = canvasWidth;
+            }
             
             // TODO 4: Part 2 - Parallax
-            
+            for(var i = 0; i < buildings.length; i++){
+                 var building = buildings[i];
+                 building.x -=.5
+                if(building.x < - 100){
+                    building.x = canvasWidth + 100
+                }
+
+            }
 
         } // end of update function - DO NOT DELETE
         
